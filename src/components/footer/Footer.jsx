28b6302c3f104477b778b7632bd92e2671e1d851
khalidnbg <khalidnabgaoui4@gmail.com>
2024-04-06 14:36:02 +0000

    create the home and the about page (ui)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Nbg</div>
+      <div className={styles.text}>
+        Nbg creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
