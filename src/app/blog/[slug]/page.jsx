8931fe472e1c6ed26aac6956f18e3382668c7dd1
khalidnbg <khalidnabgaoui4@gmail.com>
2024-04-07 03:17:12 +0000

    fetching data using api

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
+
+// FETCH DATA WITHOUT API
+// const getData = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+//   if (!res.ok) throw new Error("Something went wrong");
+//   return res.json();
+// };
 
 const SinglePostPage = async ({ params }) => {
-  console.log(params);
+  const { slug } = params;
+
+  // const post = await getData(slug);
 
   return (
     <div className={styles.container}>
@@ -10,7 +21,7 @@ const SinglePostPage = async ({ params }) => {
         <Image src={"/noavatar.png"} alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{"post title"}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             src={"/noavatar.png"}
@@ -19,16 +30,21 @@ const SinglePostPage = async ({ params }) => {
             height={50}
             className={styles.avatar}
           />
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
+
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Author</span>
             <span className={styles.detailValue}>{"NBG"}</span>
           </div>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>{"01/01/2015"}</span>
+            <span className={styles.detailValue}>{"01/01/2000"}</span>
           </div>
         </div>
-        <div className={styles.content}>{"post desc"}</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/noavatar.png" alt="" fill className={styles.img} />
+          <Image src={"/noavatar.png"} alt="" fill className={styles.img} />
         </div>
-        <span className={styles.date}>01/01/2015</span>
+        <span className={styles.date}>{"01/01/2015"}</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>{"post title"}</h1>
-        <p className={styles.desc}>{"post body"}</p>
-        <Link className={styles.link} href={"/blog/${post.slug}"}>
+        <h1 className={styles.title}>{"post.title"}</h1>
+        <p className={styles.desc}>{"post.body"}</p>
+        <Link className={styles.link} href={`/blog/${post.id}`}>
           READ MORE
         </Link>
       </div>
